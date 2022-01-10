c990c00cb9d6f882fb19a2f7b6fc611e126fc67c
Chris Hinsley <chris.hinsley@gmail.com>
2022-01-10 13:34:26 +0000

    Update app.h

--- src/lib/apps/mandelbrot/app.h
+++ src/lib/apps/mandelbrot/app.h
@@ -12,7 +12,7 @@ private:
 	const uint32_t CANVAS_WIDTH = 800;
 	const uint32_t CANVAS_HEIGHT = 800;
 	const uint32_t CANVAS_SCALE = 2;
-	const uint32_t JOB_TIMEOUT = 1000;
+	const uint32_t JOB_TIMEOUT = 5000;
 	const uint32_t JOB_LIMIT = 16;
 public:
 	Mandelbrot_App()
